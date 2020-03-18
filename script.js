ed053292a8ae05c0b9215acde52f697bec6f3987
Glitch (roman-voting) <none>
2020-03-18 21:35:21 +0000

    🦀💷 Checkpoint ./script.js:5894189/537 ./index.html:5894189/10318

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    const id = window.location.hash.split();
+    const id = window.location.hash.trim();
+
+    console.log(window.location);
+
+
+    
+    function displayOption(id) {
+        const 
+        if(id === '') {
+            $('#voting-session').hide();
+        } else {
+            $('#new-session').hide();
+        }
+    }
+
+    displayOption(id);
 });
