8c88e8934af53a85e176282f3acba930f0a0efd1
Glitch (roman-voting) <none>
2020-03-18 22:47:24 +0000

    🏈⭐️ Checkpoint ./script.js:5894189/3963

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-
-
-function AppState() {
-    this.sessionId = this.getSessionId();
-}
-
-AppState.prototype = {
-    getSessionId: function () {
-        return window.location.hash
-            .trim()
-            .replace('#', '');
-    },
-
-    setSessionId: function(sessionId) {
-        this.sessionId = sessionId;
-    },
-
-    sessionIdExists: function() {
-        return this.sessionId !== '';
+(function () {
+    function AppState() {
+        this.sessionId = '';
     }
-};
 
-function ViewManager(state) {
-    this.state = state;
+    AppState.prototype = {
+        setSessionId: function (sessionId) {
+            this.sessionId = sessionId;
+        },
 
-    this.viewIdSelectors = {
-        'voting-session': (state) => state.sessionIdExists(),
-        'new-session': (state) => !state.sessionIdExists()
+        sessionIdExists: function () {
+            return this.sessionId !== '';
+        }
     };
 
-    this.update();
-}
+    function ViewManager(state) {
+        this.state = state;
 
-ViewManager.prototype = {
-    update: function() {
-        Object.keys(this.viewIdSelectors)
-            .forEach(selector => {
-                const displayPredicate = this.viewIdSelectors[selector];
-                const element = $('#' + selector);
+        this.viewIdSelectors = {
+            'voting-session': (state) => state.sessionIdExists(),
+            'new-session': (state) => !state.sessionIdExists()
+        };
 
-                if(displayPredicate(this.state)) {
-                    element.show();
-                } else {
-                    element.hide();
-                }
-            })
+        this.update();
     }
-};
 
-$(document).ready(function () {
-    const appState = new AppState();
-    const view = new ViewManager(appState);
-});
+    ViewManager.prototype = {
+        update: function () {
+            Object.keys(this.viewIdSelectors)
+                .forEach(selector => {
+                    const displayPredicate = this.viewIdSelectors[selector];
+                    const element = $('#' + selector);
+
+                    if (displayPredicate(this.state)) {
+                        element.show();
+                    } else {
+                        element.hide();
+                    }
+                })
+        }
+    };
+
+    function App(appState, view) {
+        this.appState = appState;
+        this.view = view;
+    }
+
+    App.prototype = {
+        setSessionId: function(sessionId) {
+            this.appState.setSessionId(sessionId);
+            this.view.update();
+        },
+
+        getSessionId: function () {
+            return window.location.hash
+                .trim()
+                .replace('#', '');
+        },
+    };
+
+    $(document).ready(function () {
+        const appState = new AppState();
+        const view = new ViewManager(appState);
+
+        const app = new App(appState, view);
+    });
+})();
