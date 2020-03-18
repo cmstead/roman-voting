a48281244e943b48a04f037ae21342e7cab3d7a6
Glitch (roman-voting) <none>
2020-03-18 22:59:22 +0000

    ✌️🍤 Checkpoint ./script.js:5894189/1653

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 (function () {
+    const voteTypes = {
+        THUMBS_UP: 'thumbsUp',
+        THUMBS_SIDEWAYS: 'thumbsSideways',
+        THUMBS_DOWN: 'thumbsDown',
+    };
+
     function AppState() {
         this.sessionId = '';
+
+        this.voteCount = {
+            [voteTypes.THUMBS_UP]: 0,
+            [voteTypes.THUMBS_SIDEWAYS]: 0,
+            [voteTypes.THUMBS_DOWN]: 0
+        };
     }
 
     AppState.prototype = {
         setSessionId: function (sessionId) {
             this.sessionId = sessionId;
+        },
+
+        incrementVoteCount: function(voteType) {
+            this.voteCount[voteType]++;
         },
 
         sessionIdExists: function () {
@@ -25,7 +41,7 @@
     }
 
     ViewManager.prototype = {
-        update: function () {
+        updateMainView: function () {
             Object.keys(this.viewIdSelectors)
                 .forEach(selector => {
                     const displayPredicate = this.viewIdSelectors[selector];
@@ -43,25 +59,28 @@
     function App(appState, view) {
         this.appState = appState;
         this.view = view;
+
+        const sessionId = this.getSessionId();
+        this.setSessionId(sessionId);
     }
 
     App.prototype = {
         setSessionId: function(sessionId) {
             this.appState.setSessionId(sessionId);
-            this.view.update();
+            this.view.updateMainView();
         },
 
         getSessionId: function () {
             return window.location.hash
                 .trim()
                 .replace('#', '');
-        },
+        }
     };
 
     $(document).ready(function () {
         const appState = new AppState();
         const view = new ViewManager(appState);
 
-        const app = new App(appState, view);
+        new App(appState, view);
     });
 })();
