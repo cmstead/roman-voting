84aab7d8ecf771b698ff96259c2272918d4123d4
Glitch (roman-voting) <none>
2020-03-18 21:48:23 +0000

    🏕📻 Checkpoint ./script.js:5894189/1603

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-$(document).ready(function() {
-    const id = window.location.hash.trim();
+function getSessionId() {
+    return window.location.hash.trim().replace('#', '');
+}
 
-    console.log(window.location);
+function AppState() {
+    this.sessionId = this.getSessionId();
+}
 
+AppState.prototype = {
+    setSessionId: function() {}
+};
 
-    
+$(document).ready(function () {
+    const id = getSessionId();
+
+    const optionKeys = {
+        VOTING_SESSION: 'voting-session',
+        NEW_SESSION: 'new-session'
+    };
+
+    function doesIdExist() {}
+
     function displayOption(id) {
-        const 
-        if(id === '') {
-            $('#voting-session').hide();
-        } else {
-            $('#new-session').hide();
-        }
+        const optionToDisplay = id === ''
+            ? optionKeys.NEW_SESSION
+            : optionKeys.VOTING_SESSION;
+
+        const optionToHide = id !== ''
+            ? optionKeys.VOTING_SESSION
+            : optionKeys.NEW_SESSION;
+
+        $('#' + optionToDisplay).show();
+        $('#' + optionToHide).hide();
     }
 
     displayOption(id);
